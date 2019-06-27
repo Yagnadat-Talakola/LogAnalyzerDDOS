666ae551120e86c6ab1ff1dd367e8ee626b3ade2
Yagna Talakola <talakola.yagna@gmail.com>
2019-06-27 07:03:20 -0500

    added comments

--- src/main/java/com/processor/ddos/operations/RollingWindowOps.java
+++ src/main/java/com/processor/ddos/operations/RollingWindowOps.java
@@ -10,9 +10,9 @@ public interface RollingWindowOps {
     String ERROR_STATUS = "503";
     int REQUEST_THRESHOLD = 25000; //request count threshold
 
-    int RETENTION_PERIOD = 300; // (in seconds) retention duration of the container
+    int RETENTION_PERIOD = 300; // (in seconds) size of container in time units
     int ERROR_THRESHOLD_PERCENTAGE = 50; //503 status code percentage
-    int DURATION = 30; // (in seconds)
+    int DURATION = 30; // (in seconds) - size of Rolling Window in time units
 
     RollingWindow getOrCreateRollingWindow(ApacheLogEntry message, RollingWindowContainer container);
     void updateRollingWindow(ApacheLogEntry msg, RollingWindow rw);
